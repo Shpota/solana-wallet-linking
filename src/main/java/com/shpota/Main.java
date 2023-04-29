58f2a54835909df90c5996cab9dfb7a1a2eb2048
Sasha Shpota <5640984+Shpota@users.noreply.github.com>
2023-04-29 17:09:03 +0200

    Add wallet verification functionality (#2)

--- src/main/java/com/shpota/Main.java
+++ src/main/java/com/shpota/Main.java
@@ -1,7 +1,33 @@
 package com.shpota;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.javalin.Javalin;
+import io.javalin.plugin.bundled.CorsPluginConfig;
+import org.sol4k.Base58;
+import org.sol4k.PublicKey;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var app = Javalin.create(config -> {
+                    config.plugins.enableCors(cors ->
+                            cors.add(CorsPluginConfig::anyHost)
+                    );
+                    config.staticFiles.add("public");
+                }
+        );
+        app.post("/verify", ctx -> {
+            var request = new ObjectMapper().readValue(ctx.body(), VerificationRequest.class);
+            Boolean result = verifySignature(request.signature(), request.walletAddress());
+            ctx.result(result.toString());
+        });
+        app.start(8080);
+    }
+
+    public static Boolean verifySignature(String signature, String walletAddress) {
+        String message = "You are verifying you wallet with sol4k";
+        byte[] messageBytes = message.getBytes();
+        PublicKey publicKey = new PublicKey(walletAddress);
+        byte[] signatureBytes = Base58.decode(signature);
+        return publicKey.verify(signatureBytes, messageBytes);
     }
 }
